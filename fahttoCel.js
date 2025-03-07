179321f6d14ff51f4686bceb96e3c992f25e7c4a
Kristen <harunakristen@gmail.com>
2025-03-07 12:35:59 +0000

    more commit

--- fahttoCel.js
+++ fahttoCel.js
@@ -1,10 +1,10 @@
-Fahrenheit = (Celsius * 9) / 5 + 32;
- cToFah = (cTemp * 9) / 5 + 32;
+// Fahrenheit = (Celsius * 9) / 5 + 32;
+//  cToFah = (cTemp * 9) / 5 + 32;
 
-var fTemp = fahrenheit;
+// var fTemp = fahrenheit;
 
-function fToC(fahrenheit) {
-  var fToCel = ((fTemp - 32) * 5) / 9;
-  var message = fTemp + "\xB0F is " + fToCel + " \xB0C. ";
-  console.log(message);
-}
+// function fToC(fahrenheit) {
+//   var fToCel = ((fTemp - 32) * 5) / 9;
+//   var message = fTemp + "\xB0F is " + fToCel + " \xB0C. ";
+//   console.log(message);
+// }
